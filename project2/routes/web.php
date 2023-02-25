6c2181e6191c5997d8bab7d310028b2d4aef9e50
miroslawszyper <miroslawszyper@gmail.com>
2023-02-25 18:35:40 +0100

    Routing Laravel

--- project2/routes/web.php
+++ project2/routes/web.php
@@ -16,3 +16,51 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('homePage', function(){
+  // return view('homepage');
+  // return "WSB";
+  // return ['name' => 'WSB', 'base' => 'classic'];
+  
+  // return view('homePage', ['firstName' => 'Janusz', 'lastName' => 'Kowalski']);
+});
+  
+  Route::get('pages/{x}', function($x){
+    $pages = [
+      'contact' => "Strona WSB",
+      'home' => "Strona domowa"
+    ];
+    return $pages[$x];
+  });
+  
+  Route::get('address/{city}/{street}/{zipcode?}', function(string $city, string $street, int $zipCode = null){
+    if ($zipCode == null) {
+      $zipCode = '-';
+    }else{
+      $zipCode = substr($zipCode,0,2)."-".substr($zipCode, 2,3);
+    }
+    
+    echo <<<ADDRESS
+      Miasto: $city, ulica: $street,<br>
+      Kod pocztowy: $zipCode
+ADDRESS;
+  });
+  
+Route::redirect('adres/{city}/{street}/{zipcode?}', '/address/{city}/{street}/{zipcode?}');
+
+Route::prefix('admin')->group(function(){
+  Route::get('home/{name}', function(string $name){
+    echo "Witaj $name na stronie administracyjnej";
+  //})->where(['name' => '[a-zA-Z]+']);
+})->where(['name' => '[A-z]+']);
+  
+  Route::get('users', function(){
+    echo "Użytkownicy systemu";
+  });
+});
+
+Route::redirect('admin/{name}', '/admin/home/{name}');
+
+Route::get('homePage', function(){
+  return view('homePage', ['firstName' => 'Janusz', 'lastName' => 'Kowalski', 'city' => 'Poznań']);
+});
